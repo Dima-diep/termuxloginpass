23f2b10acceef08dfd888a9dbfab3c6c7852b9bd
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-16 19:55:08 +0400

    pass.py

---
+++ pass.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+import os
+import getpass
+#password have to be as a single word in " "
+passwd = "oldpass"
+a = getpass.getpass("Password: ")
+
+if a == passwd:
+    os.system("kill -9 python3")
+elif a != passwd:
+    print("Password incorrect")
+    os.system("python3 ~/TermPass/login.py")
