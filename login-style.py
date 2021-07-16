c75b2c7c642177fe5bdfdd8619a032b0f2828e8d
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-16 20:53:48 +0400

    login-style.py

---
+++ login-style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+import os
+
+a = arch
+
+if a == arch:
+    os.system("cat ~/termloginpass/archlinux | lolcat -S 65 -a -s 40 -i")
+elif a == debian:
+    os.system("cat debian | lolcat -S 40 -a -s 40 -i")
+elif a == kali:
+    os.system("cat kalilinux | lolcat 55 -a -s 40 -i")
+elif a == ubuntu:
+    os.system("cat ubuntu
