2a51b83acdbec28091a21184ad009854c5455672
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-16 23:39:15 +0400

    login.py

--- login.py
+++ login.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 #! -*- coding: utf-8 -*-
 import os
-import signal
 
-signal.signal(signal.SIGTSTP, signal.SIG_IGN)
+try:
+    # login have to be as a single word in " "
+    login = "oldlogin"
+    print("Login:")
+    a = input()
 
-# login have to be as a single word in " "
-login = "oldlogin"
-print("Login:")
-a = input()
-
-if a == login:
-    os.system("python3 ~/termuxloginpass/pass.py")
-elif a != login:
-    print("Login incorrect")
+    if a == login:
+        os.system("python3 ~/termuxloginpass/pass.py")
+    elif a != login:
+        print("Login incorrect")
+        os.system("python3 ~/termuxloginpass/login.py")
+except KeyboardInterrupt:
     os.system("python3 ~/termuxloginpass/login.py")
