e6db6ac40c81eb98f73dd72c3a5afb9368738803
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-16 23:32:00 +0400

    login.py

--- login.py
+++ login.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 #! -*- coding: utf-8 -*-
 import os
+import signal
+
+signal.signal(signal.SIGTSTP, signal.SIG_IGN)
 
 # login have to be as a single word in " "
 login = "oldlogin"
