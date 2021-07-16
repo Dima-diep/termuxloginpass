d463c9657ec40cc326fbb039f13c31be2c25186f
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-16 20:11:57 +0400

    addition.sh

---
+++ addition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt install tsu root-repo
+sudo chattr +i ~/termloginpass/chlogin.rb
+sudo chattr +i ~/termloginpass/chpass.rb
+sudo chattr +i ~/termloginpass/login.py
+sudo chattr +i ~/termloginpass/pass.py
