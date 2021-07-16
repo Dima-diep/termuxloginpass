872df044762123b7da646a047d517232b843f3e0
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 00:21:28 +0400

    uninstall-zsh.rb

--- uninstall-zsh.rb
+++ uninstall-zsh.rb
@@ -1,3 +1,5 @@
 File.write("/data/data/com.termux/files/usr/etc/zshrc", File.open("/data/data/com.termux/files/usr/etc/zshrc",&:read).gsub("python3 ~/termuxloginpass/login.py", " "))
 File.write("/data/data/com.termux/files/usr/etc/zshrc", File.open("/data/data/com.termux/files/usr/etc/zshrc",&:read).gsub("sudo python3 ~/termuxloginpass/login.py", " "))
 File.write("/data/data/com.termux/files/usr/etc/zshrc", File.open("/data/data/com.termux/files/usr/etc/zshrc",&:read).gsub("python3 ~/termuxloginpass/login-style.py", " "))
+File.delete("/data/data/com.termux/files/home/.linuxcolor")
+File.delete("/data/data/com.termux/files/home/termuxloginpass")
