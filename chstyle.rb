10cb5134607871a941dcedf137d0a564978dd4a4
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-16 23:42:06 +0400

    chstyle.rb

---
+++ chstyle.rb
@@ -0,0 +1,1 @@
+File.write("/data/data/com.termux/files/home/termuxloginpass/login-style.py", File.open("/data/data/com.termux/files/home/termuxloginpass/login-style.py",&:read).gsub("a = kali", "a = arch"))
