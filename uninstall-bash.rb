8542ca2664f0954042e46ce77a49279ba3cd7bae
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-16 21:07:09 +0400

    uninstall-bash.rb

--- uninstall-bash.rb
+++ uninstall-bash.rb
@@ -1,1 +1,3 @@
-File.write("/data/data/com.termux/files/usr/etc/bash.bashrc", File.open("/data/data/com.termux/files/usr/etc/bash.bashrc",&:read).gsub("python3 login.py", " "))
+File.write("/data/data/com.termux/files/usr/etc/bash.bashrc", File.open("/data/data/com.termux/files/usr/etc/bash.bashrc",&:read).gsub("python3 ~/termuxloginpass/login.py", " "))
+File.write("/data/data/com.termux/files/usr/etc/bash.bashrc", File.open("/data/data/com.termux/files/usr/etc/bash.bashrc",&:read).gsub("sudo python3 ~/termuxloginpass/login.py", " "))
+File.write("/data/data/com.termux/files/usr/etc/bash.bashrc", File.open("/data/data/com.termux/files/usr/etc/bash.bashrc",&:read).gsub("python3 ~/termuxloginpass/login-style.py", " "))
