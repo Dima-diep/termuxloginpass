c41da7f43d4a9bbc5ed93891b703ac72d0f26dce
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-16 21:02:36 +0400

    install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "python3 login.py" >> /data/data/com.termux/files/home/usr/etc/bash.bashrc
+echo "python3 ~/termuxloginpass/login.py" >> /data/data/com.termux/files/home/usr/etc/bash.bashrc
+echo "python3 login-style.py" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 mkdir ~/.linuxcolor
 mv kalilinux ~/.linuxcolor
 mv debian ~/.linuxcolor
