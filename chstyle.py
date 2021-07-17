f94baa5fb128ad2fb066dff1407b16fe5a4bc23d
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 19:33:41 +0400

    chstyle.py

--- chstyle.py
+++ chstyle.py
@@ -2,7 +2,7 @@
 #! -*- coding: utf-8 -*-
 
 with open("/data/data/com.termux/files/home/termuxloginpass/login-style.py", "r") as f:
-    raw = f.read().lower().replace("a = kali", "a = arch")
+    raw = f.read().lower().replace("a = kali:", "a = arch:"
     file = open("/data/data/com.termux/files/home/termuxloginpass/login-style.py", "w")
     file.write(raw)
     file.close()
