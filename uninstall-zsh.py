c357590ff3b5f8739bebdef7f64792f95e5166c8
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 20:20:31 +0400

    uninstall-zsh.py

--- uninstall-zsh.py
+++ uninstall-zsh.py
@@ -3,19 +3,19 @@
 import os
 
 with open("/data/data/com.termux/files/usr/etc/zshrc", "r") as f:
-    raw = f.read().lower().replace("python3 login.py", " ")
+    raw = f.read().lower().replace("python3 ~/termuxlogin/login.py", " ")
     file = open("/data/data/com.termux/files/usr/etc/zshrc", "w")
     file.write(raw)
     file.close()
     f.close()
 with open("/data/data/com.termux/files/usr/etc/zshrc", "r") as f:
-    raw = f.read().lower().replace("sudo python3 login.py", " ")
+    raw = f.read().lower().replace("sudo python3 ~/termuxlogin/login.py", " ")
     file = open("/data/data/com.termux/files/usr/etc/zshrc", "w")
     file.write(raw)
     file.close()
     f.close()
 with open("/data/data/com.termux/files/usr/etc/zshrc", "r") as f:
-    raw = f.read().lower().replace("python3 login-style.py", " ")
+    raw = f.read().lower().replace("python3 ~/termuxlogin/login-style.py", " ")
     file = open("/data/data/com.termux/files/usr/etc/zshrc", "w")
     file.write(raw)
     file.close()
