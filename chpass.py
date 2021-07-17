ffee5d9df647608be0bdf0298a6e0dbb7ff260d4
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 20:19:08 +0400

    chpass.py

--- chpass.py
+++ chpass.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 #! -*- coding: utf-8 -*-
 
+print("Old password:")
+a = input()
+print("New password:")
+b = input()
+
 with open("/data/data/com.termux/files/home/termuxloginpass/pass.py", "r") as f:
-    raw = f.read().lower().replace("oldpass", "newpass")
+    raw = f.read().lower().replace(a, b)
     file = open("/data/data/com.termux/files/home/termuxloginpass/pass.py", "w")
     file.write(raw)
     file.close()
