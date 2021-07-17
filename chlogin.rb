36f9c788b62489bb7a298ddf4bbfa03b90ea5c74
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 13:22:34 +0400

    chlogin.rb

--- chlogin.rb
+++ chlogin.rb
@@ -1,1 +1,1 @@
-File.write("/data/data/com.termux/files/home/termuxloginpass/login.py", File.open("/data/data/com.termux/files/home/termuxloginpass/login.py",&:read).gsub("newlogin", "oldlogin"))
+File.write("/data/data/com.termux/files/home/termuxloginpass/login.py", File.open("/data/data/com.termux/files/home/termuxloginpass/login.py",&:read).gsub("oldlogin", "newlogin"))
