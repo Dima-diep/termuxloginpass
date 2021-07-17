8cc3f07a5c70a1661ce9a5259b85300a71719b29
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 18:06:45 +0400

    additional-zsh.py

---
+++ additional-zsh.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+
+with open("/data/data/com.termux/files/usr/etc/zshrc", "r") as f:
+    raw = f.read().lower().replace("python3 login.py", "sudo python3 login.py")
+    file = open("/data/data/com.termux/files/usr/etc/zshrc", "w")
+    file.write(raw)
+    file.close()
+    f.close()
