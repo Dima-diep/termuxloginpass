7bb46edd1c41745b6d9c87847592735b972322f5
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 19:39:48 +0400

    login-style.py

--- login-style.py
+++ login-style.py
@@ -4,11 +4,11 @@ import os
 
 a = "kali"
 
-if a == arch:
+if a == "arch":
     os.system("cat ~/.linuxcolor/archlinux | lolcat -S 65 -a -s 40 -i")
-elif a == debian:
+elif a == "debian":
     os.system("cat ~/.linuxcolor/debian | lolcat -S 40 -a -s 40 -i")
-elif a == kali:
-    os.system("cat ~/.linuxcolor/kalilinux | lolcat 55 -a -s 40 -i")
-elif a == ubuntu:
+elif a == "kali":
+    os.system("cat ~/.linuxcolor/kalilinux | lolcat -S 55 -a -s 40 -i")
+elif a == "ubuntu":
     os.system("cat ~/.linuxcolor/Ubuntu | lolcat -S 30 -a -s 40 -i")
