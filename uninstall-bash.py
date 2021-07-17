358b0957fabb1931bc2b4a79bf6b41f2852743df
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 20:21:12 +0400

    uninstall-bash.py

--- uninstall-bash.py
+++ uninstall-bash.py
@@ -3,21 +3,21 @@
 import os
 
 with open("/data/data/com.termux/files/usr/etc/bash.bashrc", "r") as f:
-    raw = f.read().lower().replace("python3 login.py", " ")
+    raw = f.read().lower().replace("python3 ~/termuxloginpass/login.py", " ")
     file = open("/data/data/com.termux/files/usr/etc/bash.bashrc", "w")
     file.write(raw)
     file.close()
     f.close()
 
 with open("/data/data/com.termux/files/usr/etc/bash.bashrc", "r") as f:
-    raw = f.read().lower().replace("sudo python3 login.py", " ")
+    raw = f.read().lower().replace("sudo python3 ~/termuxloginpass/login.py", " ")
     file = open("/data/data/com.termux/files/usr/etc/bash.bashrc", "w")
     file.write(raw)
     file.close()
     f.close()
 
 with open("/data/data/com.termux/files/usr/etc/bash.bashrc", "r") as f:
-    raw = f.read().lower().replace("python3 login-style.py", " ")
+    raw = f.read().lower().replace("python3 ~/termuxloginpass/login-style.py", " ")
     file = open("/data/data/com.termux/files/usr/etc/bash.bashrc", "w")
     file.write(raw)
     file.close()
