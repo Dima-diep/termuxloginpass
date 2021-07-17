419db4c19a8e0aca5bcbc01d78608f3c54d63000
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 20:17:57 +0400

    chlogin.py

--- chlogin.py
+++ chlogin.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 #! -*- coding: utf-8 -*-
 
+print("Old login:")
+a = input()
+print("New login:")
+b = input()
+
 with open("/data/data/com.termux/files/home/termuxloginpass/login.py", "r") as f:
-    raw = f.read().lower().replace("oldlogin", "newlogin")
+    raw = f.read().lower().replace(a, b)
     file = open("/data/data/com.termux/files/home/termuxloginpass/login.py", "w")
     file.write(raw)
     file.close()
