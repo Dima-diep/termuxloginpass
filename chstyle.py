30a638e0a33ebe0b7b829e0f335237ef3ca9a55d
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 20:38:07 +0400

    chstyle.py

--- chstyle.py
+++ chstyle.py
@@ -4,9 +4,9 @@ print("Old style (print this: \"a = \"kali\"\"):")
 a = input()
 print("New style:")
 
-with open("/data/data/com.termux/files/home/termuxloginpass/login-style.py", r) as f:
-    raw = f.read().lower().replace("a = \"kali\"", "a = \"arch\""
-    file = open("/data/data/com.termux/files/home/termuxloginpass/login-style.py", w)
+with open("/data/data/com.termux/files/home/termuxloginpass/login-style.py", "r") as f:
+    raw = f.read().lower().replace("a = \"kali\"", "a = \"arch\"")
+    file = open("/data/data/com.termux/files/home/termuxloginpass/login-style.py", "w")
     file.write(raw)
     file.close()
     f.close()
