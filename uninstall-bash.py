04cef03d1c2a23882f12cb370e9e4a47e7a9850b
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 18:57:17 +0400

    uninstall-bash.py

---
+++ uninstall-bash.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+
+with open("/data/data/com.termux/files/usr/etc/bash.bashrc", "r") as f:
+    raw = f.read().lower().replace("python3 login.py", " ")
+    file = open("/data/data/com.termux/files/usr/etc/bash.bashrc", "w")
+    file.write(raw)
+    file.close()
+    f.close()
+
+with open("/data/data/com.termux/files/usr/etc/bash.bashrc", "r") as f:
+    raw = f.read().lower().replace("sudo python3 login.py", " ")
+    file = open("/data/data/com.termux/files/usr/etc/bash.bashrc", "w")
+    file.write(raw)
+    file.close()
+    f.close()
+
+with open("/data/data/com.termux/files/usr/etc/bash.bashrc", "r") as f:
+    raw = f.read().lower().replace("python3 login-style.py", " ")
+    file = open("/data/data/com.termux/files/usr/etc/bash.bashrc", "w")
+    file.write(raw)
+    file.close()
+    f.close()
