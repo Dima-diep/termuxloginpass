1d7d0c977c9079d97261881c055b8205c6649f70
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 19:00:37 +0400

    addition-root.sh

--- addition-root.sh
+++ addition-root.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 apt install tsu root-repo -y
-sudo chown root ~/termloginpass/chlogin.rb
-sudo chown root ~/termloginpass/chpass.rb
+sudo chown root ~/termloginpass/chlogin.py
+sudo chown root ~/termloginpass/chpass.py
 sudo chown root ~/termloginpass/login.py
 sudo chown root ~/termloginpass/pass.py
-sudo chmod 500 ~/termloginpass/chlogin.rb
-sudo chmod 500 ~/termloginpass/chpass.rb
+sudo chmod 500 ~/termloginpass/chlogin.py
+sudo chmod 500 ~/termloginpass/chpass.py
 sudo chmod 500 ~/termloginpass/login.py
 sudo chmod 500 ~/termloginpass/pass.py
-sudo chattr +i ~/termloginpass/chlogin.rb
-sudo chattr +i ~/termloginpass/chpass.rb
+sudo chattr +i ~/termloginpass/chlogin.py
+sudo chattr +i ~/termloginpass/chpass.py
 sudo chattr +i ~/termloginpass/login.py
 sudo chattr +i ~/termloginpass/pass.py
 sudo chown root ~/termloginpass
