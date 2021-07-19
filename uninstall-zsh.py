7b3253e8f0e6ef1de1d07d1474f6f948ecc6868b
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-19 14:23:46 +0400

    uninstall-zsh.py

--- uninstall-zsh.py
+++ uninstall-zsh.py
@@ -8,16 +8,26 @@ with open("/data/data/com.termux/files/usr/etc/zshrc", "r") as f:
     file.write(raw)
     file.close()
     f.close()
+
 with open("/data/data/com.termux/files/usr/etc/zshrc", "r") as f:
     raw = f.read().lower().replace("sudo python3 ~/termuxlogin/login.py", " ")
     file = open("/data/data/com.termux/files/usr/etc/zshrc", "w")
     file.write(raw)
     file.close()
     f.close()
+
 with open("/data/data/com.termux/files/usr/etc/zshrc", "r") as f:
     raw = f.read().lower().replace("python3 ~/termuxlogin/login-style.py", " ")
     file = open("/data/data/com.termux/files/usr/etc/zshrc", "w")
     file.write(raw)
     file.close()
     f.close()
+
+with open("/data/data/com.termux/files/usr/etc/zshrc", "r") as f:
+    raw = f.read().lower().replace("chmod +x * && bash exit.sh", " ")
+    file = open("/data/data/com.termux/files/usr/etc/zshrc", "w")
+    file.write(raw)
+    file.close()
+    f.close()
+
 os.system("bash ~/termuxloginpass/unadd.sh && cd ~ && rm -rf ~/.linuxcolor ~/termuxloginpass")
