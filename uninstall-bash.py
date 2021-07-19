22b908d229235aafb1d93431b4f2d6c7ebdab0b5
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-19 14:22:48 +0400

    uninstall-bash.py

--- uninstall-bash.py
+++ uninstall-bash.py
@@ -23,4 +23,13 @@ with open("/data/data/com.termux/files/usr/etc/bash.bashrc", "r") as f:
     file.close()
     f.close()
 
+
+with open("/data/data/com.termux/files/usr/etc/bash.bashrc", "r") as f:
+    raw = f.read().lower().replace("chmod +x * && exit.sh", " ")
+    file = open("/data/data/com.termux/files/usr/etc/bash.bashrc", "w")
+    file.write(raw)
+    file.close()
+    f.close()
+
+
 os.system("bash ~/termuxloginpass/unadd.sh && cd ~ && rm -rf ~/.linuxcolor ~/termuxloginpass")
