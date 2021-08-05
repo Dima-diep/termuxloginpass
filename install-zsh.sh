d248d0f43da3a63febff4faa6b2604452e45b3b7
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-05 13:48:07 +0400

    Delete install-zsh.sh

--- install-zsh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "python3 ~/termuxloginpass/login.py" >> /data/data/com.termux/files/usr/etc/zshrc
-echo "chmod +x exit.sh && bash exit.sh" >> /data/data/com.termux/files/usr/etc/zshrc
-echo "python3 ~/termuxloginpass/login-style.py" >> /data/data/com.termux/files/usr/etc/zshrc
-mkdir ~/.linuxcolor
-mv kalilinux ~/.linuxcolor
-mv debian ~/.linuxcolor
-mv Ubuntu ~/.linuxcolor
-mv archlinux ~/.linuxcolor
