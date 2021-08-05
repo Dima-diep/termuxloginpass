7b7c9c3e59b2850f8b926121edd31dca975306e1
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-05 22:49:49 +0400

    install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "#!/bin/bash" > /data/data/com.termux/files/usr/bin/login
-echo "python3 ~/termuxloginpass/login.py" >> /data/data/com.termux/files/usr/etc/login
-echo "python3 ~/termuxloginpass/login-style.py" >> /data/data/com.termux/files/usr/etc/login
+echo "python3 ~/termuxloginpass/login.py" >> /data/data/com.termux/files/usr/bin/login
+echo "python3 ~/termuxloginpass/login-style.py" >> /data/data/com.termux/files/usr/bin/login
 mkdir ~/.linuxcolor
 mv kalilinux ~/.linuxcolor
 mv debian ~/.linuxcolor
